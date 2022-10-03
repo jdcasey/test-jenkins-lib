03e059e9f2fab8a93e05273342301bf7ec233f36
John Casey <jdcasey@commonjava.org>
2022-10-03 16:12:47 -0500

    try to print env

--- src/io/github/jdcasey/OtelTest.groovy
+++ src/io/github/jdcasey/OtelTest.groovy
@@ -2,6 +2,7 @@ package io.github.jdcasey
 
 class OtelTest implements Serializable {
     def run(){
+        System.out.println(env)
         def c = Class.forName("io.opentelemetry.api.trace.Span")
     }
 }
